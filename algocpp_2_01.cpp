c7fbdbaf31cad2ecf093f362f0b6132aa05853ae
PawelZdanowicz88 <zdanowiczpawel88@gmail.com>
2023-06-04 01:25:14 +0200

    Failes added

---
+++ algocpp_2_01.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int fib(int n) {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите число n: ";
+    int n = 0;
+    std::cin >> n;
+    std::cout << "Число Фибоначчи: " << fib(n) << std::endl;
+    return 0;
+}
